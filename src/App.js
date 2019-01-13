84fb6457fe11aaaab81a4d1817916cf5c14bcfbb
telefrancais <rduffield@myseneca.ca>
2019-01-13 15:27:00 -0500

    Created a couple of functions to make sure data is being fetched.

--- src/App.js
+++ src/App.js
@@ -3,26 +3,67 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            activeLegID: '',
+            legProgress: 0
+        };
+    }
+
+    componentDidMount() {
+        this.updateLocation();
+    }
+
+    getLocation = async () => {
+        let location = await fetch('/location');
+        let json = await location.json();
+        this.setState({
+            activeLegID: json.activeLegID, 
+            legProgress: json.legProgress
+        });
+    }
+
+    updateLocation = async () => {
+        let newLocation = {
+            legID: 'CD',
+            progress: 41
+        };
+        let update = await fetch('/location', {
+            method: 'PUT',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(newLocation)
+        });
+        let json = await update.json();
+        this.setState({
+            activeLegID: json.activeLegID, 
+            legProgress: json.legProgress
+        });
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo" />
+                    <p>
+                        {this.state.legProgress}
+                    </p>
+                    <a
+                        className="App-link"
+                        href="https://reactjs.org"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        Learn React
+                    </a>
+                </header>
+            </div>
+        );
+    }
 }
 
 export default App;
